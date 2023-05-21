c59635985157a7f443e03bb61e65dfe5e4d301e6
inshal <inshal@techsoulpk.com>
2023-05-21 14:03:19 +0500

    getAccounts created

--- server/controllers/accounts.mjs
+++ server/controllers/accounts.mjs
@@ -1,18 +1,24 @@
 import { AccountModel } from "../models/Accounts.mjs"
-import { v4 as uuidv4 } from 'uuid';
 
 export const createAccount = async (req, res) => {
-    console.log({req: req.body})
     const {accountName, accountType} = req.body
     try {
-        const newAccount = await AccountModel.create({
+        await AccountModel.create({
             account_name: accountName,
             account_type: accountType
         })
-        console.log({newAccount})
         res.status(201).json({ message: 'Account Created Successfully' });
     } catch (error) {
         res.status(500).json(error.message);
     }
+}
 
+export const getAccounts = async (req, res) => {
+    try {
+        const accounts = await AccountModel.findAll()
+    
+        res.status(200).json(accounts)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
 }
--- server/routes/accounts.mjs
+++ server/routes/accounts.mjs
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { createAccount } from '../controllers/accounts.mjs'
+import { createAccount, getAccounts } from '../controllers/accounts.mjs'
 
 const router = express.Router()
 
 router.post('/', createAccount)
+router.get('/getAccounts', getAccounts)
 
 export default router
